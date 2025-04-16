48413804889b145e5c33cdbf91ba6f78fe6db050
Dip <113352667+izDeLaMo@users.noreply.github.com>
2025-04-16 21:40:21 +0000

    First pusH with tenent and landlord models

--- App/models/landlord.py
+++ App/models/landlord.py
@@ -1,2 +1,27 @@
 from App.database import db
 from .user import User
+
+class Landlord(User):
+    __tablename__ = 'landlord'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+
+  
+    def __init__(self, name, email, phone, city):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.city = city
+
+    def __repr__(self):
+        return f'<Landlord {self.name}>'
+    def get_json(self):
+        return { 'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'phone': self.phone,
+            'city': self.city
+                }
