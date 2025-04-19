b9e23bf0506999ec8fa84c94314e0bf93aa8f4a2
Dip <113352667+izDeLaMo@users.noreply.github.com>
2025-04-19 18:30:17 +0000

    reset on templates and views

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -2,6 +2,7 @@ from .user import create_user
 from App.database import db
 
 
+
 def initialize():
     db.drop_all()
     db.create_all()
--- App/views/index.py
+++ App/views/index.py
@@ -2,16 +2,10 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
-from App.controllers import (
-    get_listing,
-    get_all_listings,
-    initialize,
-)
 
 @index_views.route('/', methods=['GET'])
 def index_page():
-    listings = get_all_listings()
-    return render_template('index.html', listings=listings)
+    return render_template('index.html')
 
 @index_views.route('/init', methods=['GET'])
 def init():
@@ -21,8 +15,3 @@ def init():
 @index_views.route('/health', methods=['GET'])
 def health_check():
     return jsonify({'status':'healthy'})
-
-@index_views.route('/<int:listing_id>', methods=['GET'])
-def get_listing_page(listing_id):
-    listing = get_listing(listing_id)
-    return render_template('detail.html', listing=listing)
