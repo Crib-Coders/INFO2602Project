cade0ab1ee1a3f22aa3b5fd7e34f22198dd4f2af
Thatguybk <bradleykhan22@gmail.com>
2025-04-19 21:26:41 -0400

    added is_verified to user creation

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -6,5 +6,5 @@ from App.database import db
 def initialize():
     db.drop_all()
     db.create_all()
-    create_user('bob', 'bobpass', 'tenant', True)
+    create_user('bob', 'bobpass', 'tenant', True) #is_verified is set to False by default but bob is set to true 
     create_user('brad', 'bradpass', 'landlord')
