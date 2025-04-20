c1be69d81a1e302c6e4da22891fb6c5aa2610aec
Thatguybk <bradleykhan22@gmail.com>
2025-04-20 07:09:40 -0400

    View Details Button Fixed

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -12,5 +12,6 @@ def initialize():
     create_user('bob', 'bobpass', 'tenant', True) #is_verified is set to False by default but bob is set to true 
     create_user('brad', 'bradpass', 'landlord') #is_verified doesnt matter for landlords
     create_listing(2, 'Apartment 1', 1, 1, 1000, 'New York', 'images/apartment1.jpg', 'Nice') #create listing for brad
+    create_listing(2, 'Apartment 2', 1, 1, 1000, 'New York', 'images/apartment1.jpg', 'Nice')
     create_review(1, 1, 'Great place!', 5) #create review for listing 1 by tenant 1
      #create review for listing 1 by tenant 2
